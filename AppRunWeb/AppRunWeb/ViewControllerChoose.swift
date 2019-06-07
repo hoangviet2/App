f2da27572dec0d2d5e2e599dd5fa9ee916e1370e
Viet Hoang <hoangviet18306@gmail.com>
2019-06-07 21:55:54 +0700

    Frist commit

---
+++ AppRunWeb/AppRunWeb/ViewControllerChoose.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerChoose.swift
+//  AppRunWeb
+//
+//  Created by Hoang Viet on 6/6/19.
+//  Copyright © 2019 Hoang Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerChoose: UIViewController {
+    @IBAction func btn_Back(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
