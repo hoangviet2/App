ed9442a4084337b7be5ad236d3760bbf01460ee3
Viet Hoang <hoangviet18306@gmail.com>
2020-01-18 22:59:04 +0700

    commit homepage and Featured tab

---
+++ Landmark/Landmark/HomePage.swift
@@ -0,0 +1,74 @@
+//
+//  HomePage.swift
+//  Landmark
+//
+//  Created by Hoang Viet on 12/31/19.
+//  Copyright © 2019 Hoang Viet. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+    
+    @State var showingProfile = false
+    
+    var profileButton: some View {
+        Button(action: { self.showingProfile.toggle() }) {
+            Image(systemName: "person.crop.circle")
+                .imageScale(.large)
+                .accessibility(label: Text("User Profile"))
+                .padding()
+        }
+    }
+    
+    var body: some View {
+        NavigationView{
+            List {
+                FeaturedLandmarks(landmarks: featured)
+                .scaledToFill()
+                .frame(height: 200)
+                .clipped()
+                .listRowInsets(EdgeInsets())
+                
+                ForEach(categories.keys.sorted(), id: \.self) { key in
+                    HomePageRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+                NavigationLink(destination: LandmarkList()) {
+                    Text("See All")
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing: profileButton)
+            .sheet(isPresented: $showingProfile) {
+                Text("User Profile")
+            }
+        }
+    }
+}
+
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image.resizable()
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+        .environmentObject(UserData())
+    }
+}
--- Landmark/Landmark/LandmarkList.swift
+++ Landmark/Landmark/LandmarkList.swift
@@ -9,15 +9,34 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @EnvironmentObject var userData: UserData
+    
     var body: some View {
-        List(landmarkData, id: \.Id) { landmark in
-            LandmarkRow(landmark: landmark)
+        
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorites only")
+            }
+
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
         }
+        .navigationBarTitle(Text("Landmarks"))
+        
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        NavigationView {
+            LandmarkList()
+                .environmentObject(UserData())
+        }
     }
 }
